514f25259baeb0adf616f314ef1c6df649ca39ce
fndh <Xavier@10.192.94.75>
2017-04-03 11:03:09 +0200

    Added multiple OS support with x32 or x64

--- CoToSe/scripts/mysql_unix.sh
+++ CoToSe/scripts/mysql_unix.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-distroutput = $(uname -a)
-#Check distro to see if apt-get or anything else
+DISTR_OUTPUT = $(uname -a)
+#Check distro to see if apt-get or something else
 if [[ 'Ubuntu' == *distroutput* ]]; then
     GET = 'apt-get install'
 fi
 
 #Install package
-sudo $(GET) mysql-server
+sudo ${GET} mysql-server
 if [ $? != 0 ]; then
     echo "Something went wrong with the installation"
     exit
